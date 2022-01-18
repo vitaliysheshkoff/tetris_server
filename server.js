87b3223f09b7b9bbf0343db1f9293baf07a5ceaa
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-01-19 00:54:53 +0300

    update code for deploy process

--- server.js
+++ server.js
@@ -80,4 +80,4 @@ io.on('connection', (socket) => {
 
 })
 
-http.listen(8080, () => console.log('Server running on port 8080'))
+http.listen(process.env.PORT || 8080, () => console.log('Server running on port 8080'))
